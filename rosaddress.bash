622b29327cfe6b9c30192ec5d8a70d9ac936f028
seiya0412 <seiya.shimizu+github@gmail.com>
2021-11-10 17:36:22 +0900

    replaced invalid 'ROS_HOST_NAME' with 'ROS_HOSTNAME'. ref: 'http://wiki.ros.org/ROS/EnvironmentVariables'

--- rosaddress.bash
+++ rosaddress.bash
@@ -5,13 +5,13 @@ function _func_rosserver() {
 	else
 		echo "There are Ethernet connection"
 		export ROS_MASTER_URI=http://${ip_addr}:11311
-		export ROS_HOST_NAME=${ip_addr}
+		export ROS_HOSTNAME=${ip_addr}
 		export ROS_IP=${ip_addr}
 		export PS1="\[\033[41;1;33m\]<ROS_server>\[\033[0m\]\w$ "
 	fi
 
 	env | grep "ROS_MASTER_URI"
-	env | grep "ROS_HOST_NAME"
+	env | grep "ROS_HOSTNAME"
 	env | grep "ROS_IP"
 }
 
@@ -25,29 +25,29 @@ function _func_rosclient() {
 		else
 			echo "There are Ethernet connection"
 			export ROS_MASTER_URI=http://$1:11311
-			export ROS_HOST_NAME=${ip_addr}
+			export ROS_HOSTNAME=${ip_addr}
 			export ROS_IP=${ip_addr}
 			export PS1="\[\033[44;1;33m\]<ROS_client>\[\033[0m\]\w$ "
 		fi
 		env | grep "ROS_MASTER_URI"
-		env | grep "ROS_HOST_NAME"
+		env | grep "ROS_HOSTNAME"
 		env | grep "ROS_IP"
 	fi
 }
 
 function _func_roslocal() {
 	export ROS_MASTER_URI=http://localhost:11311
-	unset ROS_HOST_NAME
+	unset ROS_HOSTNAME
 	unset ROS_IP
 	export PS1="\[\033[42;1;33m\]<ROS_local>\[\033[0m\]\w$ "
 	env | grep "ROS_MASTER_URI"
-	env | grep "ROS_HOST_NAME"
+	env | grep "ROS_HOSTNAME"
 	env | grep "ROS_IP"
 }
 
 function _func_rosexit(){
 	export ROS_MASTER_URI=http://localhost:11311
-	unset ROS_HOST_NAME
+	unset ROS_HOSTNAME
 	unset ROS_IP
 	export PS1="\u@\h:\w\\$ "
 }
